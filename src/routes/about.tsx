ea476b56e3a47e6805bdee0137113284a625b0d5
anushm55 <71762231004@cit.edu.in>
2025-09-10 15:35:50 +0530

    update about

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -17,7 +17,8 @@ about.get('/', (c) => {
         <div style="text-align:center">
           <p>I'm an Undergrad student studying at <a href="https://www.cit.edu.in/">CIT</a></p>
           <p>I like tinkering with computers</p>
-          <p>Find me at <a href="https://github.com/Anushm55">Github</a> <a href="https://www.linkedin.com/in/anushm55/">LinkedIn</a> <a href="https://mastodon.online/@peepeepoopooo">Mastodon</a> </p>
+          <p>My Socials - <a href="https://github.com/Anushm55">Github</a> <a href="https://www.linkedin.com/in/anushm55/">LinkedIn</a> <a href="https://mastodon.online/@peepeepoopooo">Mastodon</a> </p>
+          <p>Contact : <a href="mailto:hello@anushm55.com">send me an e-mail</a></p>
         </div>
       </Content>
     </Layout>
